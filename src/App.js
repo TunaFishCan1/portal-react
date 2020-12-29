61272cad415a65d8e4b7302ef95efc30568131f9
TunaFishCan1 <39428374+TunaFishCan1@users.noreply.github.com>
2020-12-29 11:56:22 +0900

    added some basic functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 function App() {
+  const history = useHistory();
+  const [apiResponse, resChange] = useState("");
+
+  function callAPI() {
+    fetch("http://127.0.1.1:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => resChange(res));
+  }
+
+  function respond() {
+    fetch("http://127.0.1.1:9000/testAPI", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: "johnny" }),
+    })
+      .then((res) => res.text())
+      .then((res) => resChange(res));
+  }
+  const naver = () => {
+    let path = `https://www.naver.com/`;
+    history.push(path);
+  };
+  useEffect(() => {
+    callAPI();
+    respond();
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{apiResponse}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,6 +44,14 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <br></br>
+          <button type="button" onClick={this.naver}>
+            Naver
+          </button>
+          <br></br>
+          <button type="button">Daum</button>
+          <br></br>
+          <button type="button">Nate</button>
         </a>
       </header>
     </div>
