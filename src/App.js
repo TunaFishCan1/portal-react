68e57a415b8de73f4b94c917367fe9a55b52c489
TunaFishCan1 <39428374+TunaFishCan1@users.noreply.github.com>
2021-01-04 09:57:09 +0900

    Added material-ui table

--- src/App.js
+++ src/App.js
@@ -8,12 +8,79 @@ import {
   mobileVendor,
   mobileModel,
 } from "react-device-detect";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+const columns = [
+  { id: "addr", label: "접속주소", minWidth: 170 },
+  { id: "meth", label: "접속방법", minWidth: 100 },
+  {
+    id: "time",
+    label: "접속시각",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("ko-KR"),
+  },
+  {
+    id: "model",
+    label: "기종",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("ko-KR"),
+  },
+  {
+    id: "manu",
+    label: "제조사",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("ko-KR"),
+  },
+];
+function createData(addr, meth, time, model, manu) {
+  return { addr, meth, time, model, manu };
+}
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
 const publicIp = require("public-ip");
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+/>;
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+/>;
 
 function App() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   const [apiResponse, resChange] = useState("");
   const [OS, changeInfo] = useState("");
   const [IP, getIP] = useState("");
+  const [rows, cArray] = useState([]);
   (async () => {
     getIP(await publicIp.v4());
   })();
@@ -69,8 +136,8 @@ function App() {
       },
       body: JSON.stringify(json),
     })
-      .then((res) => res.text())
-      .then((res) => resChange(res));
+      .then((res) => res.json())
+      .then((res) => cArray(res.data));
   }
   return (
     <div className="App">
@@ -95,6 +162,59 @@ function App() {
           {apiResponse}
         </p>
       </header>
+      <Paper className={classes.root}>
+        <TableContainer className={classes.container}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.code}
+                    >
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === "string"
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
     </div>
   );
 }
